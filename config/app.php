b9fa8098e9d94aad60b667a25042e68961ce3cdf
Mai Văn Toàn <toan.mv0823@gmail.com>
2018-12-20 09:41:05 +0700

    Require Laravel debug bar

--- config/app.php
+++ config/app.php
@@ -229,4 +229,10 @@ return [
         
     ],
 
+    'debug_blacklist' => [
+        '_COOKIE' => array_keys($_COOKIE),
+        '_SERVER' => array_keys($_SERVER),
+        '_ENV' => array_keys($_ENV),        
+    ],
+
 ];
