f2f642b35004bcbe96e33fbe697e45d49143eec9
Mai Văn Toàn <toan.mv0823@gmail.com>
2018-12-28 11:00:20 +0700

    Create Document Version

---
+++ app/Http/Requests/DocumentVersionRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DocumentVersionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'bail|required|min:3|unique:document_versions',
+        ];
+    }
+
+    /**
+    * Get the error messages for the defined validation rules.
+    *
+    * @return array
+    */
+    public function messages()
+    {
+        return [
+            'name.required' => __('Name Document Version is required'),
+            'name.min'  => __('Name must be min 3 characters'),
+            'name.unique'  => __('Name has been taken'),
+        ];
+    }
+}
